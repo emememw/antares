471d97dd8feeb49e50f5a9d13931cc0562f2788f
markus-wi <emveyh@googlemail.com>
2013-02-16 12:05:26 +0100

    new test map

---
+++ antares/src/com/emveyh/antares/map/TestGameMap.java
@@ -0,0 +1,34 @@
+package com.emveyh.antares.map;
+
+import java.util.Random;
+
+import com.emveyh.antares.core.GlobalConfig;
+import com.emveyh.antares.entity.EntityManager;
+
+public class TestGameMap extends GameMap {
+
+	public TestGameMap() {
+		super(100, 100);
+		generate();
+	}
+	
+	public void generate() {
+		Random random = new Random();
+		for(int x = 0; x < this.getWidth(); x++) {
+			for(int y = 0; y < this.getHeight(); y++) {
+				int chance = random.nextInt(100);
+				if(chance < 20) {
+					this.getTiles()[x][y] = Tile.WALL;
+				} else {
+					EntityManager.getInstance().getPlayer().setX(x*GlobalConfig.FIXED_TILESIZE);
+					EntityManager.getInstance().getPlayer().setY(y*GlobalConfig.FIXED_TILESIZE);
+					this.getTiles()[x][y] = Tile.FLOOR;
+				}
+			}
+		}
+		
+		
+		
+	}
+	
+}
